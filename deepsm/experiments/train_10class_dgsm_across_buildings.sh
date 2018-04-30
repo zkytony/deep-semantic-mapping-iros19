b7c1d1d52a94c838fbb6b4b6b150b105dc6527e7
Kaiyu Zheng <kaiyutony@gmail.com>
2018-04-30 15:47:54 -0700

    Wrote shell script to mass train dgsm for across building scenario

---
+++ deepsm/experiments/train_10class_dgsm_across_buildings.sh
@@ -0,0 +1,37 @@
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'CR'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'1PO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'2PO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'DW'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'LO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'LAB'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'BA'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'KT'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'MR'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Stockholm_Freiburg-Saarbrucken', 'submodel_class':'UT'}"
+
+
+
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'CR'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'1PO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'2PO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'DW'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'LO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'LAB'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'BA'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'KT'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'MR'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Freiburg_Saarbrucken-Stockholm', 'submodel_class':'UT'}"
+
+
+
+
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'CR'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'1PO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'2PO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'DW'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'LO'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'LAB'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'BA'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'KT'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'MR'}"
+./train_test_dgsm_single_class.py DGSM_ACROSS_BUILDINGS --config "{'test_case':'Saarbrucken_Freiburg-Stockholm', 'submodel_class':'UT'}"

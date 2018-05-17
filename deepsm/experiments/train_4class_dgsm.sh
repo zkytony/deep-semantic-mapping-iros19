8be69626d3ab5bb9c04aa5d4e448458b696a2e00
Kaiyu Zheng <kaiyutony@gmail.com>
2018-05-17 10:02:27 -0700

    Updated commands in shell scripts to run mass tests

--- deepsm/experiments/train_4class_dgsm.sh
+++ deepsm/experiments/train_4class_dgsm.sh
@@ -1,19 +1,58 @@
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'456-7', 'submodel_class': 'CR'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'456-7', 'submodel_class': '1PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'456-7', 'submodel_class': '2PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'456-7', 'submodel_class': 'DW'}"
+# Stockholm
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'456-7', 'submodel_class': 'CR'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'456-7', 'submodel_class': '1PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'456-7', 'submodel_class': '2PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'456-7', 'submodel_class': 'DW'}"
 
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'457-6', 'submodel_class': 'CR'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'457-6', 'submodel_class': '1PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'457-6', 'submodel_class': '2PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'457-6', 'submodel_class': 'DW'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'457-6', 'submodel_class': 'CR'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'457-6', 'submodel_class': '1PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'457-6', 'submodel_class': '2PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'457-6', 'submodel_class': 'DW'}"
 
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'467-5', 'submodel_class': 'CR'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'467-5', 'submodel_class': '1PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'467-5', 'submodel_class': '2PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'467-5', 'submodel_class': 'DW'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'467-5', 'submodel_class': 'CR'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'467-5', 'submodel_class': '1PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'467-5', 'submodel_class': '2PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'467-5', 'submodel_class': 'DW'}"
 
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'567-4', 'submodel_class': 'CR'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'567-4', 'submodel_class': '1PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'567-4', 'submodel_class': '2PO'}"
-./train_test_dgsm_single_class.py Stockholm DGSM_SAME_BUILDING --config "{'test_case':'567-4', 'submodel_class': 'DW'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'567-4', 'submodel_class': 'CR'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'567-4', 'submodel_class': '1PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'567-4', 'submodel_class': '2PO'}"
+./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Stockholm --config "{'test_case':'567-4', 'submodel_class': 'DW'}"
+
+# # Freiburg
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'12-3', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'12-3', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'12-3', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'12-3', 'submodel_class': 'DW'}"
+
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'13-2', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'13-2', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'13-2', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'13-2', 'submodel_class': 'DW'}"
+
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'23-1', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'23-1', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'23-1', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Freiburg --config "{'test_case':'23-1', 'submodel_class': 'DW'}"
+
+
+# # Saarbrucken
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'123-4', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'123-4', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'123-4', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'123-4', 'submodel_class': 'DW'}"
+
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'124-3', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'124-3', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'124-3', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'124-3', 'submodel_class': 'DW'}"
+
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'134-2', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'134-2', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'134-2', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'134-2', 'submodel_class': 'DW'}"
+
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'234-1', 'submodel_class': 'CR'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'234-1', 'submodel_class': '1PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'234-1', 'submodel_class': '2PO'}"
+# ./train_test_dgsm_single_class.py DGSM_SAME_BUILDING -d Saarbrucken --config "{'test_case':'234-1', 'submodel_class': 'DW'}"

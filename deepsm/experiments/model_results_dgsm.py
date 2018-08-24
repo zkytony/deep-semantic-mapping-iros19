1fc080efe814a4ce6fa0054d367266a9517f5719
Kaiyu Zheng <kaiyutony@gmail.com>
2018-08-24 08:14:58 -0700

    Added script to organize dgsm results

--- deepsm/experiments/model_results_dgsm.py
+++ deepsm/experiments/model_results_dgsm.py
@@ -1,29 +1,72 @@
 #!/usr/bin/env python3
-#
-# Generate result for a DGSM that combines several submodels;
-# Specifically, we would like to produce arrays of normalized
-# likelihood values.
-#
-# In this experiment framework, the virtual scans of each graph
-# is grouped into the same room. Therefore, when saving the results
-# of likelihoods, we save one file per room (i.e. per graph). These
-# will later act as input to GraphSPN.
-#
-# We report the DGSM's classification result per node, and accuracy
-# 1) per graph per class,   2) per graph all classes in total,
-# 3) all graphs per class,  4) all graphs all classes in total.
-#
-# Refer to dgsm/result.py:Result:get_classification_results_for_graphs()
 
+import numpy as np
 import os
 import argparse
-import deepsm.dgsm.model_results as dgsm_results
 import deepsm.util as util
 import deepsm.experiments.paths as paths
+import yaml
+from pprint import pprint
+
+def same_building_results(db_name, config):
+    """
+    Configurations:
+    "test_case": (str) e.g. '456-7'
+    "trial": (int) e.g. 4    (if left out, compute average and standard deviation over all trials)
+
+    Returns: A dictionary that contains "avg", "std", and "class_results"
+    as its keys. If 'trial' is present in config, then the `std` represents
+    standard deviation across graphs in that trial. Otherwise, it represents
+    standard deviation across trials.
+    """
+    trial = 0 if "trial" not in config else config['trial']
+    
+    results_dir = paths.path_to_dgsm_result_same_building(util.CategoryManager.NUM_CATEGORIES,
+                                                          db_name,
+                                                          "graphs",
+                                                          trial,
+                                                          config['test_case'].split("-")[0],
+                                                          config['test_case'].split("-")[1])
 
 
-if __name__ == "__main__":
+    if "trial" in config:
+        # Read the results.yaml in the specified trial
+        with open(os.path.join(results_dir, "results.yaml")) as f:
+           stats = yaml.load(f)
 
+        # Get standard deviation across graphs
+        accuracies = []
+        for key in stats:
+            if key.startswith(db_name.lower()):
+                accuracies.append(stats[key]['accuracy'])
+        std = np.std(accuracies)
+        return {'avg': stats['accuracy'],
+                'std': std,
+                'class_results': stats['class_results']}
+
+    else:
+        # Read results.yaml in all trials.
+        accuracies = []
+        class_results = {}
+        for root, dirs, files in os.walk(os.path.dirname(results_dir)):
+            for name in files:
+                if name.endswith("results.yaml"):
+                    with open(os.path.join(root, name)) as f:
+                        stats = yaml.load(f)
+                    accuracies.append(stats['accuracy'])
+                    for catg in stats['class_results']:
+                        if catg not in class_results:
+                            class_results[catg] = []
+                        class_results[catg].append(stats['class_results'][catg][-1])
+        for catg in class_results:
+            class_results[catg] = {'avg': np.mean(class_results[catg]),
+                                   'std': np.std(class_results[catg])}
+        return {'avg': np.mean(accuracies),
+                'std': np.std(accuracies),
+                'class_results': class_results}
+                    
+ 
+def main():
     parser = argparse.ArgumentParser(description="Get DGSM test results for graphs")
     parser.add_argument('what', type=str, help='what data you want to make available constants: (DGSM_SAME_BUILDING, DGSM_ACROSS_BUILDINGS)')
     parser.add_argument('-d', '--db_name', type=str, help='e.g. Freiburg (case-sensitive)')
@@ -39,65 +82,16 @@ if __name__ == "__main__":
     tmp_data_dir = ".tmp_experiment_dgsm"
     os.makedirs(tmp_data_dir, exist_ok=True)
 
+    if "category_type" in config:
+        util.CategoryManager.TYPE = config['category_type']
+    else:
+        util.CategoryManager.TYPE = "SIMPLE"
+    util.CategoryManager.init()
+
     if what == "DGSM_SAME_BUILDING":
-        """
-        Configurations:
-        "test_case": (str) e.g. '456-7'
-        """
-        original_real_data_path = os.path.join(paths.path_to_dgsm_dataset_same_building(util.CategoryManager.NUM_CATEGORIES,
-                                                                                        args.db_name), 'real_data')
-        original_set_defs_path = os.path.join(os.path.dirname(original_real_data_path),
-                                              config['test_case'], "set_defs")
-        symlink_real_data_path = os.path.join(tmp_data_dir, "real_data")
-        symlink_set_defs_path = os.path.join(tmp_data_dir, "set_defs")
-
-        if os.path.exists(symlink_real_data_path):
-            os.remove(symlink_real_data_path)
-        if os.path.exists(symlink_set_defs_path):
-            os.remove(symlink_set_defs_path)
-        os.symlink(original_real_data_path, symlink_real_data_path)
-        os.symlink(original_set_defs_path, symlink_set_defs_path)
-
-        results_dir = os.path.dirname(paths.path_to_dgsm_result_same_building(util.CategoryManager.NUM_CATEGORIES,
-                                                                              args.db_name,
-                                                                              "CR",
-                                                                              config['test_case'].split("-")[0],
-                                                                              config['test_case'].split("-")[1]))
-
-    elif what == "DGSM_ACROSS_BUILDINGS":
-        """
-        Configurations:
-        "test_case": (str) e.g. 'Stockholm_Freiburg-Saarbrucken'
-        """
-        test_building = config['test_case'].split("_")[0]
-        train_buildings = sorted(config['test_case'].split("_")[1].split("-"))
-        
-        original_real_data_path = os.path.join(paths.path_to_dgsm_dataset_across_buildings(util.CategoryManager.NUM_CATEGORIES),
-                                               'real_data')
-        original_set_defs_path = os.path.join(os.path.dirname(original_real_data_path),
-                                              config['test_case'], "set_defs")
-        symlink_real_data_path = os.path.join(tmp_data_dir, "real_data")
-        symlink_set_defs_path = os.path.join(tmp_data_dir, "set_defs")
-
-        if os.path.exists(symlink_real_data_path):
-            os.remove(symlink_real_data_path)
-        if os.path.exists(symlink_set_defs_path):
-            os.remove(symlink_set_defs_path)
-        os.symlink(original_real_data_path, symlink_real_data_path)
-        os.symlink(original_set_defs_path, symlink_set_defs_path)
-
-        results_dir = os.path.dirname(paths.path_to_dgsm_result_across_buildings(util.CategoryManager.NUM_CATEGORIES,
-                                                                                 "CR",
-                                                                                 train_buildings,
-                                                                                 test_building))
-    classes = []
-    for i in range(util.CategoryManager.NUM_CATEGORIES):
-        classes.append(util.CategoryManager.category_map(i, rev=True))
+        stats = same_building_results(args.db_name, config)
+        pprint(stats)
 
 
-    dgsm_args_parser = dgsm_results.create_parser()
-    dgsm_args = dgsm_results.parse_args(parser=dgsm_args_parser,
-                                        args_list=[tmp_data_dir,
-                                                   results_dir,
-                                                   '1'])
-    dgsm_results.main(args=dgsm_args, trained_classes=classes)
+if __name__ == "__main__":
+    main()

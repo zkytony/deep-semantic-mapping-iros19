c1bfa2788fcdaf14b9952c3947b12680f88b6101
Kaiyu Zheng <kaiyutony@gmail.com>
2018-09-01 11:58:33 -0700

    Added column of trial name in some results

--- deepsm/experiments/analysis/dgsm/merge_reports.py
+++ deepsm/experiments/analysis/dgsm/merge_reports.py
@@ -1,11 +1,13 @@
 import os
 
 result = []
-for filename in os.listdir("./"):
+for filename in sorted(os.listdir("./")):
     if (filename.startswith("full-report")):
         with open(filename) as f:
             lines = f.readlines()
             if len(result) == 0:
+                if not lines[0].startswith("trial_name"):
+                    lines.insert(0, "trial_name")
                 result.append(lines[0])
             result.append(lines[1])
 

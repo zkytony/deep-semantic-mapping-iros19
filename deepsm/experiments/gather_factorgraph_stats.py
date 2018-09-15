9c3467071fda29d3a65b5df2362d41b9e5b2c94d
Kaiyu Zheng <kaiyutony@gmail.com>
2018-09-15 01:16:31 -0700

    Modified gather factorgrpah stats too

--- deepsm/experiments/gather_factorgraph_stats.py
+++ deepsm/experiments/gather_factorgraph_stats.py
@@ -7,6 +7,21 @@ import numpy as np
 from deepsm.experiments.common import BP_RESULTS_ROOT
 from pprint import pprint
 
+def get_seq_id(case_name):
+    return case_name.split("-")[-1]
+
+
+def complete_the_report(report):
+    """Used for older report.log format"""
+    
+    # Compute accuracy by class
+    accuracy_per_catg = []
+    for catg in report:
+        if not catg.startswith("_"):
+            accuracy_per_catg.append(report[catg][2])
+    report['_overall_by_class_'] = float(np.mean(accuracy_per_catg))
+    report['_stdev_by_class_'] = float(np.std(accuracy_per_catg))
+
 def main():
     parser = argparse.ArgumentParser(description="Gather stats for graphspn experiments; A test case" \
                                      "is located at [ExperimentName]/[TestCase]_timestamp_[test-name]_seq_id")
@@ -18,8 +33,8 @@ def main():
 
     results_dir = os.path.join(BP_RESULTS_ROOT, args.exp_name, args.test_case + "_" + args.test_name)
 
-    stats = {'factor_graph':{'total':0, 'correct':0, 'overall':0.0}}
-    
+    stats = {'factor_graph':{'_all_':{'_overall_':0.0}}}
+
     acc_by_class = []
     acc_overall = []
     
@@ -29,34 +44,50 @@ def main():
            and args.test_name in case_name:
             print("    " + case_name)
 
+            seq_id = get_seq_id(case_name)
+            floor = seq_id.split("_")[0]
+            if floor not in stats['factor_graph']:
+                stats['factor_graph'][floor] = {}
+            
             # Open report.log
             with open(os.path.join(results_dir, case_name, 'report.log')) as f:
                 report = yaml.load(f)
 
-            stats['factor_graph'][case_name] = report
+            if "_overall_by_class_" not in report:
+                complete_the_report(report)
+
+            stats['factor_graph'][floor][seq_id] = report
 
             acc_by_class.append(report['_overall_by_class_'])
             acc_overall.append(report['_overall_'])
 
-            fg_total = report['_total_inferred_']
-            fg_correct = report['_total_correct_']
+    stats['factor_graph']['_per_floor_'] = {}
+    for floor in stats['factor_graph']:
+        if floor.startswith("_"):
+            continue
+        fl_acc_by_class = []
+        fl_acc_overall = []
+        for seq_id in stats['factor_graph'][floor]:
+            fl_acc_by_class.append(stats['factor_graph'][floor][seq_id]['_overall_by_class_'])
+            fl_acc_overall.append(stats['factor_graph'][floor][seq_id]['_overall_'])
 
-            
-            with open(os.path.join(results_dir, case_name, 'report.log')) as f:
-                report = yaml.load(f)
-
-            # For now, just compute the overall
-            fg_total = report['_total_inferred_']
-            fg_correct = report['_total_correct_']
-            stats['factor_graph']['total'] += fg_total
-            stats['factor_graph']['correct'] += fg_correct
-            
-    stats['factor_graph']['overall'] = np.mean(acc_overall)
-    stats['factor_graph']['stdev'] = np.std(acc_overall)
-    stats['factor_graph']['overall_by_class'] = np.mean(acc_by_class)
-    stats['factor_graph']['stdev_by_class'] = np.std(acc_by_class)
+        stats['factor_graph']['_per_floor_'][floor] = {
+            'overall': np.mean(fl_acc_overall),
+            'stdev':  np.std(fl_acc_overall),
+            'overall_by_class': np.mean(fl_acc_by_class),
+            'stdev_by_class': np.std(fl_acc_by_class),
+        }
+        
+    stats['factor_graph']['_all_']['_overall_'] = np.mean(acc_overall)
+    stats['factor_graph']['_all_']['_stdev_'] = np.std(acc_overall)
+    stats['factor_graph']['_all_']['_overall_by_class_'] = np.mean(acc_by_class)
+    stats['factor_graph']['_all_']['_stdev_by_class_'] = np.std(acc_by_class)
 
     pprint(stats)
+    print("Factor_Graph per floor:")
+    pprint(stats['factor_graph']['_per_floor_'])
+    print("Factor_Graph overall:")
+    pprint(stats['factor_graph']['_all_'])
 
 if __name__ == "__main__":
     main()
